d8ac1972c84fa03d52cafe4afd1d8f7ca8c79d97
tonyleesos <tonyleesos@hotmail.com>
2017-01-31 22:38:36 +0800

    Add files via upload

---
+++ autocamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class autocamera : MonoBehaviour {
+
+	public float baseWidth = 1024;
+	public float baseHeight = 768;
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake(){
+		mainCamera.aspect = this.baseWidth / this.baseHeight;
+//		float newSize = (float)Screen.height / (float)Screen.width * 1024 / 768 * 5;
+//		mainCamera.orthographicSize = Mathf.Max ( newSize, 5 );
+	}
+
+}
---
+++ autocanvas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class autocanvas : MonoBehaviour {
+
+
+
+
+	//public CanvasScaler canvasScaler;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+//		float screenWidthScale = Screen.width / canvasScaler.referenceResolution.x;
+//		float screenHidthScale = Screen.height / canvasScaler.referenceResolution.y;
+//		canvasScaler.matchWidthOrHeight = screenWidthScale > screenHidthScale ? 1 : 0;
+	}
+
+	void Awake(){
+		CanvasScaler canvasScaler = GetComponent<CanvasScaler> ();
+		float screenWidthScale = Screen.width / canvasScaler.referenceResolution.x;
+		float screenHeighScale = Screen.height / canvasScaler.referenceResolution.y;
+
+		canvasScaler.matchWidthOrHeight = screenWidthScale > screenHeighScale ? 1 : 0;
+	}
+
+}
